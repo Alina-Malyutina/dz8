b0d83c993a95edfe1f98628e31adf2fc14751004
Alina Malyutina <linamalutina1@gmail.com>
2022-11-27 21:18:09 +0300

    add task 58 - multiply matrixes

---
+++ task60/Program.cs
@@ -0,0 +1,32 @@
+﻿
+void PrintArrayWithIndex(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int m = 0; m < array.GetLength(2); m++)
+            {
+            Console.Write($"{array[i, j, m]} ({i}, {j}, {m})" );
+
+            }
+            Console.WriteLine();
+        }
+    Console.WriteLine();
+    }
+}
+
+int [,,] array = new int [3, 3, 3];
+for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int m = 0; m < array.GetLength(2); m++)
+            {
+            array [i,j,m] = new Random().Next(4,10) * new Random().Next(3,10);
+            }
+        }
+    }
+PrintArrayWithIndex(array);
+
+
