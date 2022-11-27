90965a7be46b3712c7064a433dcfdf4fe113a5ed
Alina Malyutina <linamalutina1@gmail.com>
2022-11-27 19:15:35 +0300

    add task 54 - sort array

---
+++ task54/Program.cs
@@ -0,0 +1,48 @@
+﻿void SortTheRowsInDescendingOrder(int[,] array)
+{
+   int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)      
+            {
+                if(array[i,k] > array[i,j])
+                {
+                    temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+void FillArrayRowColByRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = new int[5, 5];
+FillArrayRowColByRandom(array);
+PrintArray(array);
+Console.WriteLine();
+SortTheRowsInDescendingOrder(array);
+PrintArray(array);
