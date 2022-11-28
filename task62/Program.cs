fdb9ff948906de15bc4231eb598531c26ba9f653
Alina Malyutina <linamalutina1@gmail.com>
2022-11-28 17:16:11 +0300

    add task 62 - spiral

---
+++ task62/Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int n = 4;
+int[,] result = new int[n, n];
+int numbers = 1;
+int i = 0;
+int j = 0;
+
+while (numbers <= n * n)
+{
+    result[i, j] = numbers;
+    if (i <= j + 1 && i + j < n - 1)
+        ++j;
+    else if (i < j && i + j >= n - 1)
+        ++i;
+    else if (i >= j && i + j > n - 1)
+        --j;
+    else
+        --i;
+    ++numbers;
+}
+PrintArray(result);
